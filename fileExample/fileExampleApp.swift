6df09d159e32a9da1b4819015d3e6050402ebc50
yukuji <liane.jbm@gmail.com>
2023-06-27 09:44:59 -0600

    Initial Commit

---
+++ fileExample/fileExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  fileExampleApp.swift
+//  fileExample
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct fileExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
